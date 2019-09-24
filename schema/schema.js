72f2fa92d90c0e9c6fe77627e93492e63cea6d0e
Sam Morgan <sammorgan@Sam-Morgan.localdomain>
2019-09-24 10:17:07 -0600

    added second schema for categories

--- schema/schema.js
+++ schema/schema.js
@@ -2,24 +2,43 @@ var _ = require('lodash');
 
 const graphql = require('graphql');
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLID
+ } = graphql;
 
 //dummy data
 
 let recipes=[
-    {id:'1', name:'pancakes', link:'https://www.allrecipes.com/recipe/21014/good-old-fashioned-pancakes/', category: 'breakfast'},
-    {id:'2', name:'baked potatoes', link:'https://www.spendwithpennies.com/perfect-baked-potatoes/', category: 'dinner'},
-    {id:'3', name:'pb&j', link:'https://www.allrecipes.com/recipe/49943/grilled-peanut-butter-and-jelly-sandwich/', category: 'lunch '}
+    {id:'1', name:'pancakes', link:'https://www.allrecipes.com/recipe/21014/good-old-fashioned-pancakes/', category: 'vegetarian'},
+    {id:'2', name:'baked potatoes', link:'https://www.spendwithpennies.com/perfect-baked-potatoes/', category: 'vegan'},
+    {id:'3', name:'pb&j', link:'https://www.allrecipes.com/recipe/49943/grilled-peanut-butter-and-jelly-sandwich/', category: 'lunch'}
+];
+
+let categories=[
+    {id:'1', type:'vegetarian'},
+    {id:'2', type:'vegan'},
+    {id:'3', type:'lunch'}
 ];
 
 
 const RecipeType = new GraphQLObjectType({
     name: 'Recipe',
     fields: () => ({
-        id: { type:GraphQLString },
+        id: { type:GraphQLID},
         name: { type:GraphQLString },
         link: { type:GraphQLString },
         category: { type:GraphQLString },
+    })
+});
+
+const CategoryType = new GraphQLObjectType({
+    name: 'Category',
+    fields: () => ({
+        id: { type:GraphQLID},
+        type: { type:GraphQLString },
     })
 });
 
@@ -28,12 +47,21 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         recipe: {
             type: RecipeType,
-            args: { id: { type:GraphQLString } },
+            args: { id: { type:GraphQLID } },
             resolve(parent, args){
-                //code to get data from db/other source
                 return _.find(recipes,{id:args.id});
             }
+        },
+        category: {
+            type: CategoryType,
+            args: { id: { type:GraphQLID } },
+            resolve(parent, args){
+                return _.find(categories,{id:args.id});
+            }
+
+
         }
+
     }
 });
 
